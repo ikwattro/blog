05fbac50c287a41103ae987eef9b130a039fd436
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-01-14 14:22:20 +0100

    added specific tpl for posts

--- render.sh
+++ render.sh
@@ -4,7 +4,7 @@ files=sources/*
 for f in $files
   do
     echo "Rendering file $f"
-    asciidoctor -T _templates -D articles/ "$f"
+    asciidoctor -T _templates/post -D articles/ "$f"
   done
 echo "Rendering Index"
 asciidoctor -T _templates index.adoc
